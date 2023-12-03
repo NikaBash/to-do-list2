5523263797f9170810d7558b6173e640e49ef34c
NikaBash <veronika-0509@yandex.com>
2023-12-03 16:02:02 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const inputField = document.querySelector('.inputField');
+const btn = document.querySelector('.btn');
+const toDoContainer = document.querySelector('.toDoContainer');
+
+btn.addEventListener('click', () => {
+    const item = document.createElement('li');
+    item.innerText = inputField.value;
+    if(inputField.value.length === 0) {
+        return false;
+    }
+    item.classList.add('toDoAdded');
+    toDoContainer.appendChild(item);
+    inputField.value = '';
+
+    item.addEventListener('click', () => {
+        item.classList.add('toDoCompleted');
+    })
+    
+    item.addEventListener('dblclick', () => {
+        toDoContainer.removeChild(item);
+    })
+
+})
+
+
+
+//#eeaeca, #83ebd1   #008759, #f7006c 9653b9 399c7b
